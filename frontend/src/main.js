a4ce0be7879be81416ffc788c7f8978c11feabbe
BorealisCode <53436057+BorealisCode@users.noreply.github.com>
2019-07-30 18:09:09 +0200

    Norwegian_translation
    Norwegian_translation_main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,17 +7,14 @@ Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
 // Import all locales
-import locale_da from "./i18n/da.json";
 import locale_en from "./i18n/en.json";
 import locale_es from "./i18n/es.json";
-import locale_fr from "./i18n/fr.json";
 import locale_hr from "./i18n/hr.json";
 import locale_it from "./i18n/it.json";
 import locale_ja from "./i18n/ja.json";
 import locale_nl from "./i18n/nl.json";
 import locale_pl from "./i18n/pl.json";
 import locale_pt from "./i18n/pt.json";
-import locale_ro from "./i18n/ro.json";
 import locale_sl from "./i18n/sl.json";
 import locale_sv from "./i18n/sv.json";
 
@@ -29,17 +26,15 @@ Bridge.Start(() => {
     const i18n = new VueI18n({
       locale: result, // set locale
       messages : {
-        da: locale_da,
         en: locale_en,
         es: locale_es,
-        fr: locale_fr,
         hr: locale_hr,
         it: locale_it,
         ja: locale_ja,
         nl: locale_nl,
+        no: locale_no,
         pl: locale_pl,
-        pt: locale_pt,
-        ro: locale_ro,
+        pt: locale_pt, 
         sl: locale_sl,
         sv: locale_sv,
       },
