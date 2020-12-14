f06fb1f551b03716a4bf2e6f7f1db355bf6f2f26
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2020-12-14 08:29:22 +0100

    Add debug builds to the build scripts

--- build.bat
+++ build.bat
@@ -12,6 +12,10 @@ DEL vertcoin-ocm.exe
 wails build
 CD build 
 7z a ../vertcoin-ocm-%ver%-windows-x64.zip vertcoin-ocm.exe 
+CD ..
+wails build -d
+CD build 
+7z a ../vertcoin-ocm-%ver%-windows-x64-debug.zip vertcoin-ocm.exe 
 CD ../tracking
 DEL version.go
 REN version.go.build version.go
--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@ mv tracking/version.go tracking/version.go.dev
 echo "package tracking" > tracking/version.go
 echo "var version=\"$1-$GITVER\"" >> tracking/version.go
 wails build
-rm tracking/version.go
-mv tracking/version.go.dev tracking/version.go 
-cd build 
+cd build
 zip ../vertcoin-ocm-$1-linux-x64.zip ./vertcoin-ocm
+cd ..
+wails build -d 
+cd build
+zip ../vertcoin-ocm-$1-linux-x64-debug.zip ./vertcoin-ocm
+cd ..
+rm tracking/version.go
+mv tracking/version.go.dev tracking/version.go
