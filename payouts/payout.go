bdde11121e8eede75084d097d6492bd2a3d891cd
Erik Husby <husby.erik@gmail.com>
2021-02-20 00:59:18 -0600

    Convert displayed average earning to payout coin

--- payouts/payout.go
+++ payouts/payout.go
@@ -1,5 +1,12 @@
 package payouts
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/vertiond/verthash-one-click-miner/util"
+)
+
 type Payout interface {
 	GetID() int
 	GetName() string
@@ -31,4 +38,37 @@ func GetPayout(payout int, testnet bool) Payout {
 		}
 	}
 	return payouts[0]
+}
+
+func GetBitcoinPerUnitCoin(coinName string, coinTicker string) float64 {
+	jsonPayload := map[string]interface{}{}
+	err := util.GetJson(fmt.Sprintf("https://api.coingecko.com/api/v3/exchanges/bittrex/tickers?coin_ids=%s", strings.ToLower(coinName)), &jsonPayload)
+	if err != nil {
+		return 0.0
+	}
+	jsonTickersArr, ok := jsonPayload["tickers"].([]interface{})
+	if !ok {
+		return 0.0
+	}
+
+	result := 0.0
+	for _, jsonTickerInfo := range jsonTickersArr {
+		jsonTickerInfoMap := jsonTickerInfo.(map[string]interface{})
+		jsonTickerBase, ok1 := jsonTickerInfoMap["base"]
+		jsonTickerTarget, ok2 := jsonTickerInfoMap["target"]
+		if !ok1 || !ok2 {
+			continue
+		}
+		if jsonTickerBase == coinTicker && jsonTickerTarget == "BTC" {
+			jsonTickerConvertedLast, ok := jsonTickerInfoMap["converted_last"].(map[string]interface{})
+			if ok {
+				jsonTickerConvertedLastBTC, ok := jsonTickerConvertedLast["btc"].(float64)
+				if ok {
+					result = jsonTickerConvertedLastBTC
+				}
+			}
+			break
+		}
+	}
+	return result
 }
