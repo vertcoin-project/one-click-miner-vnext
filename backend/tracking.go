a7c7c38cda977783fd9fe61c24bcbbfa0157f364
vertiond <35286924+vertiond@users.noreply.github.com>
2021-03-17 23:19:35 -0500

    Add payout information link

--- backend/tracking.go
+++ backend/tracking.go
@@ -1,6 +1,8 @@
 package backend
 
 import (
+	"fmt"
+
 	"github.com/vertcoin-project/one-click-miner-vnext/tracking"
 	"github.com/vertcoin-project/one-click-miner-vnext/util"
 )
@@ -22,4 +24,14 @@ func (m *Backend) TrackingEnabled() string {
 
 func (m *Backend) ReportIssue() {
 	util.OpenBrowser("https://github.com/vertcoin-project/one-click-miner-vnext/issues/new")
+}
+
+func (m *Backend) PayoutInformation() {
+	var mining_address string
+	if m.UseCustomPayout() {
+		mining_address = m.zergpoolAddress
+	} else {
+		mining_address = m.walletaddress
+	}
+	util.OpenBrowser(fmt.Sprintf("https://zergpool.com/?address=%s", mining_address))
 }
--- frontend/src/components/Tracking.vue
+++ frontend/src/components/Tracking.vue
@@ -8,6 +8,7 @@
       </span> -
       <span>{{ $t('tracking.tracking_enabled') }}.&nbsp;</span>
       <a @click="disableTracking">{{ $t('tracking.disable_tracking') }}</a> -
+      <a @click="payoutInformation">{{ $t('tracking.payout_information') }}</a>
       <a @click="reportIssue">{{ $t('tracking.report_issue') }}</a>
     </p>
     <p v-if="!tracking">
@@ -18,6 +19,10 @@
       </span> -
       <span>{{ $t('tracking.tracking_disabled') }}.&nbsp;</span>
       <a @click="enableTracking">{{ $t('tracking.enable_tracking') }}</a>
+      <span>
+        &nbsp;-
+        <a @click="payoutInformation">{{ $t('tracking.payout_information') }}</a>
+      </span>
       <span>
         &nbsp;-
         <a @click="reportIssue">{{ $t('tracking.report_issue') }}</a>
@@ -53,6 +58,9 @@ export default {
   methods: {
     update: function() {
       this.$emit("update");
+    },
+    payoutInformation: function() {
+      window.backend.Backend.PayoutInformation();
     },
     reportIssue: function() {
       window.backend.Backend.ReportIssue();
