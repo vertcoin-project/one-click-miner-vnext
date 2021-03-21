f9469bc969d74872e6dbedbb5839e2b104ca55d6
vertiond <35286924+vertiond@users.noreply.github.com>
2021-03-21 13:51:23 -0500

    Add HashCryptos

---
+++ pools/hashcryptos.go
@@ -0,0 +1,57 @@
+package pools
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/vertiond/verthash-one-click-miner/util"
+)
+
+var _ Pool = &HashCryptos{}
+
+type HashCryptos struct {
+	LastFetchedPayout time.Time
+	LastPayout        uint64
+}
+
+func NewHashCryptos() *HashCryptos {
+	return &HashCryptos{}
+}
+
+func (p *HashCryptos) GetPendingPayout(addr string) uint64 {
+	jsonPayload := map[string]interface{}{}
+	err := util.GetJson(fmt.Sprintf("https://www.hashcryptos.com/api/walletEx/?address=%s", addr), &jsonPayload)
+	if err != nil {
+		return 0
+	}
+	vtc, ok := jsonPayload["unpaid"].(float64)
+	if !ok {
+		return 0
+	}
+	vtc *= 100000000
+	return uint64(vtc)
+}
+
+func (p *HashCryptos) GetStratumUrl() string {
+	return "stratum+tcp://stratum3.hashcryptos.com:9991"
+}
+
+func (p *HashCryptos) GetPassword() string {
+	return "x"
+}
+
+func (p *HashCryptos) GetID() int {
+	return 6
+}
+
+func (p *HashCryptos) GetName() string {
+	return "HashCryptos.com"
+}
+
+func (p *HashCryptos) GetFee() float64 {
+	return 0.90
+}
+
+func (p *HashCryptos) OpenBrowserPayoutInfo(addr string) {
+	util.OpenBrowser(fmt.Sprintf("https://hashcryptos.com"))
+}
