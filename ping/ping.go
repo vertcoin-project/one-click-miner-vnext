99836559eaa2b2df8574d49c39b3fb6d5cf9ce1f
KforG <34801641+KforG@users.noreply.github.com>
2021-06-19 21:40:38 +0200

    Requested changes, using structs etc.
    Requested changes by gertjaap as of PR #365 includes:
    Saving nodes inside an array of structs.
    Renamed functions and varible names.
    Removed unnecessary functions.
    New way of sorting nodes.
    
    Removed fr1

--- ping/ping.go
+++ ping/ping.go
@@ -13,27 +13,6 @@ import (
 	"github.com/vertcoin-project/one-click-miner-vnext/util"
 )
 
-//To add your node to the list simply add it to the nodes variable with the correct syntax and change the index number to relect the current number of nodes
-//Your node can have a combined max fee of currently 2%, it will not be selected if it doesn't meet that requirement
-//Your node needs to be pingable (Allow ICMP pings)
-//Your node should be live 24/7 to avoid disrupting miners.
-
-var nodes = []string{
-	"fr1.vtconline.org",
-	"p2proxy.vertcoin.org",
-	"mindcraftblocks.com",
-	"vtc-fl.javerity.com",
-	"vtc-ca.javerity.com",
-	"vtc.p2pminers.nl",
-	"p2p-usa.xyz",
-	"p2p-ekb.xyz",
-	"asia.p2p-spb.xyz",
-	"p2p-spb.xyz",
-	"p2p-south.xyz",
-	"siberia.mine.nu",
-}
-var NodeResults = make(map[time.Duration]string)
-
 type Conditions struct {
 	MaxFee      float64
 	MaxMiners   int
@@ -45,7 +24,7 @@ type Conditions struct {
 var Set = Conditions{
 	MaxFee:      2,
 	MaxMiners:   35,
-	MaxNextPing: 30 * time.Millisecond,
+	MaxNextPing: 25 * time.Millisecond, //Prevents the miner from using a node with too high latency compared to other nodes. Should prevent the miner from using nodes outside of their region
 	PingPackets: 2,
 	PingTimeout: 2000000000,
 }
@@ -55,75 +34,82 @@ type SelectedNode struct {
 	P2PoolURL     string
 }
 
-var Node SelectedNode
+var Selected SelectedNode
 
-func GetNode(testnet bool) {
+func GetSelectedNode(testnet bool) {
 	if testnet {
-		Node = SelectedNode{
+		Selected = SelectedNode{
 			P2PoolStratum: networks.Active.P2ProxyStratum,
 			P2PoolURL:     networks.Active.P2ProxyURL,
 		}
 	} else {
-		Node = SelectedNode{
-			P2PoolStratum: getClosestNodeStratum(),
-			P2PoolURL:     getClosestNodeURL(),
-		}
+		selector()
 	}
 }
 
-var selectedNode string = selector()
-
-func selector() (selected string) {
-	_, err := getNodeInformation("127.0.0.1:9171")
+func selector() {
+	_, err := GetNodeInformation("http://127.0.0.1:9171/")
 	if err == nil {
 		logging.Infof("Selected local p2pool node\n")
-		selected = "127.0.0.1:9171"
-		return selected
+		Selected = SelectedNode{
+			P2PoolStratum: "stratum+tcp://127.0.0.1:9171",
+			P2PoolURL:     "http://127.0.0.1:9171/",
+		}
 	} else {
 		logging.Infof("No local node detected, selecting other public nodes\n")
-	}
 
-	err = pingNodes()
-	if err != nil {
-		logging.Warnf("Nodes could not be pinged, selecting random node")
-		rand.Seed(time.Now().Unix())
-		selected = nodes[rand.Intn(len(nodes))]
-		logging.Infof("%s has been randomly selected", selected)
-		return selected
-	}
-	SortedPingSlice := SortPingtimes()
-
-	for i := 0; i < len(SortedPingSlice); i++ {
-		NodeInformation, _ := getNodeInformation(NodeResults[SortedPingSlice[i]])
-		Fee := CheckFee(NodeInformation)
-		if Fee {
-			CurrentMiners := CheckCurrentMiners(NodeInformation)
-			if CurrentMiners {
-				selected = NodeResults[SortedPingSlice[i]]
-				logging.Infof("%s selected, fulfilled all requirements\n", NodeResults[SortedPingSlice[i]])
-				break
-			} else { //If the node fulfills the max fee requirement but there is more than the set MaxNextping to the next node it will select the current
-				DetermineNextPingTime := SortedPingSlice[i+1]
-				DetermineNextPingTime -= SortedPingSlice[i]
-				if DetermineNextPingTime > Set.MaxNextPing {
-					selected = NodeResults[SortedPingSlice[i]]
-					logging.Infof("%s selected, next node has too high ping time\n", NodeResults[SortedPingSlice[i]])
-					break
-				}
-				logging.Warnf("%s had more than %v miners, trying new inorder to retain efficiency\n", SortedPingSlice[i], Set.MaxMiners)
+		err = PingNodes()
+		if err != nil {
+			logging.Warnf("Nodes could not be pinged, selecting random node\n")
+			rand.Seed(time.Now().Unix())
+			randInt := rand.Intn(len(NodeList))
+			Selected = SelectedNode{
+				P2PoolStratum: NodeList[randInt].Stratum,
+				P2PoolURL:     NodeList[randInt].URL,
 			}
+			logging.Infof("%s has been randomly selected\n", NodeList[randInt].Hostname)
 		} else {
-			logging.Warnf("%s had more than a %f fee, trying new\n", SortedPingSlice[i], Set.MaxFee)
+
+			sort.Slice(NodeList, func(i, j int) bool {
+				return NodeList[i].PingTime < NodeList[j].PingTime
+			})
+
+			for i := 0; i < len(NodeList); i++ {
+				nodeInformation, _ := GetNodeInformation(NodeList[i].URL)
+				fee := CheckFee(nodeInformation)
+				if fee {
+					currentMiners := CheckCurrentMiners(nodeInformation)
+					if currentMiners {
+						Selected = SelectedNode{
+							P2PoolStratum: NodeList[i].Stratum,
+							P2PoolURL:     NodeList[i].URL,
+						}
+						logging.Infof("%s selected, fulfilled all requirements\n", NodeList[i])
+						break
+					} else { //If the node fulfills the max fee requirement but there is more than the set MaxNextping to the next node it will select the current
+						determineNextPingTime := NodeList[i+1].PingTime
+						determineNextPingTime -= NodeList[i].PingTime
+						if determineNextPingTime > Set.MaxNextPing {
+							Selected = SelectedNode{
+								P2PoolStratum: NodeList[i].Stratum,
+								P2PoolURL:     NodeList[i].URL,
+							}
+							logging.Infof("%s selected, next node has too high ping time\n", NodeList[i])
+							break
+						}
+						logging.Warnf("%s had more than %v miners, trying new inorder to retain efficiency\n", NodeList[i], Set.MaxMiners)
+					}
+				} else {
+					logging.Warnf("%s is either unreachable or had more than a %f fee, trying new\n", NodeList[i], Set.MaxFee)
+				}
+			}
 		}
 	}
-	return selected
 }
 
-func pingNodes() error {
-	results := make([]time.Duration, len(nodes))
-
-	for i := 0; i < len(nodes); i++ {
-		pinger, err := ping.NewPinger(nodes[i])
+func PingNodes() error {
+	for i := 0; i < len(NodeList); i++ {
+		pinger, err := ping.NewPinger(NodeList[i].Hostname)
 		pinger.SetPrivileged(true)       //This line is needed for windows because of ICMP
 		pinger.Timeout = Set.PingTimeout //Sets the time for which the pinger will timeout regardless of how many packets there has been recieved
 		if err != nil {
@@ -138,31 +124,15 @@ func pingNodes() error {
 			logging.Warn(err)
 			return err
 		}
-		if nodes[i] == "p2proxy.vertcoin.org" { //Currently this node uses port 9172 instead of 9171, if this changes this statement can be removed and 9171 can be added to all nodes
-			nodes[i] += ":9172"
-		} else {
-			nodes[i] += ":9171"
-		}
-		results[i] = pinger.Statistics().AvgRtt
-		NodeResults[results[i]] = nodes[i]
-		logging.Infof("%s: %v \n", nodes[i], results[i])
+		NodeList[i].PingTime = pinger.Statistics().AvgRtt
+		logging.Infof("%s: %v \n", NodeList[i], NodeList[i].PingTime)
 	}
 	return nil
 }
 
-func SortPingtimes() []time.Duration {
-	SortedMap := make([]time.Duration, 0, len(NodeResults))
-	for k := range NodeResults {
-		SortedMap = append(SortedMap, k)
-	}
-	sort.Slice(SortedMap, func(i, j int) bool { return SortedMap[i] < SortedMap[j] })
-	return SortedMap
-}
-
-func getNodeInformation(Node string) (jsonPayload map[string]interface{}, err error) {
-	NodeURL := "http://"
-	NodeURL += Node
-	err = util.GetJson(fmt.Sprintf("%s/local_stats", NodeURL), &jsonPayload)
+//Instead of making a http request to the node each time we need to get information, we do it once and then reuse the collected data.
+func GetNodeInformation(NodeURL string) (jsonPayload map[string]interface{}, err error) {
+	err = util.GetJson(fmt.Sprintf("%slocal_stats", NodeURL), &jsonPayload)
 	if err != nil {
 		logging.Errorf("Unable to fetch node information\n", err.Error())
 		return jsonPayload, err
@@ -188,25 +158,9 @@ func CheckFee(jsonPayload map[string]interface{}) bool {
 
 //To ensure efficiency of the selected p2pool node a limit of miners has been put in place, returns true if the number is equal to Maxminers or below
 func CheckCurrentMiners(jsonPayload map[string]interface{}) bool {
-	CurrentMiners, ok := jsonPayload["miner_hash_rates"].(string)
-	if !ok {
-		return false
-	}
-	if len(CurrentMiners) > Set.MaxMiners {
+	currentMiners, _ := jsonPayload["miner_hash_rates"].(string)
+	if len(currentMiners) > Set.MaxMiners {
 		return false
 	}
 	return true
-}
-
-func getClosestNodeStratum() (stratum string) {
-	stratum = "stratum+tcp://"
-	stratum += selectedNode
-	return stratum
-}
-
-func getClosestNodeURL() (URL string) {
-	URL = "http://"
-	URL += selectedNode
-	URL += "/"
-	return URL
 }
