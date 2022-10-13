97314d8720a9fab86f67749e2722b3a2c3d06e86
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2022-10-13 07:31:24 +0200

    Fix compile issue

--- pools/bbqpool.go
+++ pools/bbqpool.go
@@ -19,21 +19,49 @@ func NewBBQPool(addr string) *BBQPool {
 	return &BBQPool{Address: addr}
 }
 
+func (p *BBQPool) GetJsonMember(val interface{}, keys []string) (interface{}, bool) {
+	for _, k := range keys {
+		valMap, ok := val.(map[string]interface{})
+		if !ok {
+			return nil, false
+		}
+
+		val, ok = valMap[k]
+		if !ok {
+			return nil, false
+		}
+	}
+	return val, true
+}
+
 func (p *BBQPool) GetPendingPayout() uint64 {
 	jsonPayload := map[string]interface{}{}
 	err := util.GetJson(fmt.Sprintf("https://bbqpool.org/api/vertcoin/miners?method=%s", p.Address), &jsonPayload)
 	if err != nil {
 		return 0
 	}
-	generate, ok := jsonPayload["body"]["primary"]["payments"]["generate"].(float64)
+
+	generateRaw, ok := p.GetJsonMember(jsonPayload, []string{"body","primary","payments","generate"})
 	if !ok {
 		return 0
 	}
-	immature, ok := jsonPayload["body"]["primary"]["payments"]["immature"].(float64)
-	if !ok {
+
+	immatureRaw, ok := p.GetJsonMember(jsonPayload, []string{"body","primary","payments","immature"})
+    if !ok {
 		return 0
 	}
-	vtc = (generate + immature) * 100000000
+
+	generate, ok := generateRaw.(float64)
+    if !ok {
+		return 0
+	}
+
+	immature, ok := immatureRaw.(float64)
+    if !ok {
+		return 0
+	}
+	
+	vtc := (generate + immature) * 100000000
 	return uint64(vtc)
 }
 
@@ -61,6 +89,6 @@ func (p *BBQPool) GetFee() float64 {
 	return 1.0
 }
 
-func (p *BBQPool) OpenBrowserPayoutInfo(addr string) {
-	util.OpenBrowser(fmt.Sprintf("https://bbqpool.org/", addr))
+func (p *BBQPool) OpenBrowserPayoutInfo(_addr string) {
+	util.OpenBrowser("https://bbqpool.org/")
 }
