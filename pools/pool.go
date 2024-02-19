cde33ad276992ed30d2b33a33e776d64e07eb4de
decentralisedcoder <57637916+decentralisedcoder@users.noreply.github.com>
2024-02-19 08:50:22 +0000

    Cleaning up
    Cleaning up removed pools from master repo

--- pools/pool.go
+++ pools/pool.go
@@ -23,11 +23,8 @@ func GetPools(addr string, testnet bool) []Pool {
 		NewSuprnova(addr),
 		NewP2Pool(addr),
 		Newzpool(addr),
-		NewBBQPool(addr),
-		NewAcidpool(addr),
 		NewMiningpoolSweden(addr),
 		NewCoinMinerz(addr),
-		NewMiningcorePool(addr),
 	}
 }
 
